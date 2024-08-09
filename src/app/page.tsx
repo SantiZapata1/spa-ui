37d0995ebc9f8b4c2d44b48d94083df3881209ef
Santiago Zapata <santiago@MacBook-Pro-de-Santiago.local>
2024-08-09 11:28:34 -0300

    agregue los colores del logo en otro.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,23 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import NavBar from "./components/NavBar/NavBar";
+import "./otro.css"
+
+//componente principal
 export default function Home() {
   return (
     <main>
+
+      {/* barra de navegacion */}
       <NavBar/>
+
+      <div className='vinoLogo'> hola</div>
+      <div className='greenLogo'> hola</div>
+
+
+
+
+
     </main>
   );
 }
