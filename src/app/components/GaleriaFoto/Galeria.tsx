774afa0099f5e3a5bde188ec1ead9baf56e2979a
MSCV2607 <maurosebavecc@gmail.com>
2024-08-27 23:40:18 -0300

    Se agrega cardServi, se agrega servicios a la pagina principal, se agrega mas espacio para fotos y se arregla el Carrusel

--- src/app/components/GaleriaFoto/Galeria.tsx
+++ src/app/components/GaleriaFoto/Galeria.tsx
@@ -15,14 +15,15 @@ const Galeria = () => {
   return (
     <div className="py-8 bg-gray-100">
       <h2 className="text-4xl font-bold mb-8 text-center">Galería</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 px-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 px-4">
         {images.map((src, index) => (
-          <div key={index} className="relative w-full h-64 overflow-hidden">
+          <div key={index} className="relative w-full h-64 overflow-hidden rounded-lg">
             <Image
               src={src}
               alt={`Imagen ${index + 1}`}
-              fill 
-              className="object-cover rounded-lg" 
+              fill
+              className="object-cover"
+              sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, 20vw" // Ajusta el tamaño en función del viewport
             />
           </div>
         ))}
@@ -32,6 +33,3 @@ const Galeria = () => {
 };
 
 export default Galeria;
-
-
-
