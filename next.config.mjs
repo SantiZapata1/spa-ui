737a4dfb0cb024598fa0061a89954928f42be8a7
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-09-11 07:54:52 -0300

    rip

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,14 @@ const nextConfig = {
   reactStrictMode: true,
     images: {
         domains: ['https://spa-ui.onrender.com/'], // reemplaza con tu dominio de Render
-        remotePatterns: [
-            { 
-                protocol: 'http' || 'https',
-                hostname: 'localhost',
-                port: '4000',
-                pathname: '/api/images/**'
-            } 
-        ],
+        // remotePatterns: [
+        //     { 
+        //         protocol: 'http' || 'https',
+        //         hostname: 'localhost',
+        //         port: '4000',
+        //         pathname: '/api/images/**'
+        //     } 
+        // ],
     },
 };
 
