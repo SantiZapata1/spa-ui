7b5a3f97d51f5dfa7001a436f2e235d10c985d20
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-09-11 07:47:30 -0300

    Dalee

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
     images: {
+        domains: ['https://spa-ui.onrender.com/'], // reemplaza con tu dominio de Render
         remotePatterns: [
             { 
                 protocol: 'http' || 'https',
