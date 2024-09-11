e007d40b6859f0d3650c7c779f1db8fce8457280
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-09-10 22:54:14 -0300

    Vuelvan a cruzar los dedos

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      // Uncomment the following line to build a static site.
+  output: "export",
+  reactStrictMode: true,
     images: {
         remotePatterns: [
             { 
