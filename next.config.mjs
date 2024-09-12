209bcc1ac2be11f686681663e7ee2013983825dc
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-09-12 09:16:26 -0300

    Funciona porfa

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
       // Uncomment the following line to build a static site.
-  //output: "export",
+  output: "export",
   reactStrictMode: true,
     images: {
-        domains: ['https://spa-ui.onrender.com/'], // reemplaza con tu dominio de Render
+        domains: ['https://spa-ui.onrender.com/' || "http://api.spa.sentirse-bien.gonzaloebel.tech/"], // reemplaza con tu dominio de Render
         // remotePatterns: [
         //     { 
         //         protocol: 'http' || 'https',
