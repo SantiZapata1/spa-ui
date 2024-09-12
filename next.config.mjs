5784f3c11cdbe058ae4455582bba8f2d8494536b
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-09-12 11:16:35 -0300

    Noticias fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,15 @@ const nextConfig = {
       // Uncomment the following line to build a static site.
   reactStrictMode: true,
     images: {
-        domains: ['https://spa-ui.onrender.com/' || "http://api.spa.sentirse-bien.gonzaloebel.tech/"], // reemplaza con tu dominio de Render
-        // remotePatterns: [
-        //     { 
-        //         protocol: 'http' || 'https',
-        //         hostname: 'localhost',
-        //         port: '4000',
-        //         pathname: '/api/images/**'
-        //     } 
-        // ],
+        domains: ['https://spa-ui.onrender.com/' || "https://api.spa.sentirse-bien.gonzaloebel.tech/"], // reemplaza con tu dominio de Render
+        remotePatterns: [
+            { 
+                protocol: 'http' || 'https',
+                hostname: 'localhost' || "api.spa.sentirse-bien.gonzaloebel.tech",
+                port: '4000' || process.env.PORT,
+                pathname: '/api/images/**'
+            } 
+        ],
     },
     ...(PRODUCCION && { output: "export" }), // Condicionalmente agregar la propiedad output
 };
