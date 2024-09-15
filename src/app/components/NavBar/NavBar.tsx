22a8af153dc230be39675bc86811e4bbb33082df
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2024-09-15 20:52:35 -0300

    Las redes se ocultan en el navbar mobile

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,18 +1,20 @@
-"use client";
+'use client';
 
 import React, { useState } from 'react';
 import Image from "next/image";
 import Link from 'next/link';
-import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp } from "react-icons/fa";
+// import logo from "/logo_sin_fondo.png";
 import { useAuth } from './../../../context/auth';
 import NavBarOptions from './NavBarOptions';
-import NavBarMobile from './NavBarMobile';
+import NavBarMobileUpdated from './NavBarMobile';
 
 import { Bars3Icon } from '@heroicons/react/20/solid';
+import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 export default function NavBar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [userDropwdownOpen, setUserDropdownOpen] = useState(false);
+  const [dropOptionsOnMobile, setDropOptionsOnMobile] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -29,20 +31,22 @@ export default function NavBar() {
 
   const { signIn, errorsAuth, user, isAuthenticated } = useAuth();
 
-  const [dropOptionsOnMobile, setDropOptionsOnMobile] = useState(false);
-
   return (
     <div className='flex flex-col md:flex-row w-full items-center z-50 relative'>
       <div className='flex md:flex-row justify-between p-2 md:justify-around w-full items-center space-y-4 md:space-y-0 md:space-x-6 py-1'>
         <Link href="/" onClick={closeDropdown}>
           <div className='w-40'>
-            <img src="loguitodef.png" alt="logo del spa" />
+            <img
+              src="loguitodef.png"
+              alt="logo del spa" 
+            />
           </div>
         </Link>
         <div className='hidden md:flex'>
           <NavBarOptions />
         </div>
-        <div className='flex space-x-4'>
+        {/* Redes Sociales */}
+        <div className='hidden md:flex space-x-4'>
           <a href="https://facebook.com" className="text-pink-700 hover:text-gray-800">
             <FaFacebookF className="w-5 h-5" />
           </a>
@@ -60,7 +64,7 @@ export default function NavBar() {
       </div>
       {dropOptionsOnMobile && (
         <div className='flex flex-col md:hidden'>
-          <NavBarMobile setDropOptionsOnMobile={setDropOptionsOnMobile}/>
+          <NavBarMobileUpdated setDropOptionsOnMobile={setDropOptionsOnMobile} />
         </div>
       )}
     </div>
