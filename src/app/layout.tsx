31141c3fd7a0f9089afa067b4e753c48a44cbf58
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2024-10-11 12:30:35 -0300

    Se agrega nueva carpeta llamada proyecto2 sin Footer ni navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,32 @@
-import type { Metadata } from "next";
+"use client"; // Marca este componente como Client Component
+
 import { Inter } from "next/font/google";
+import { usePathname } from "next/navigation";
 import "./globals.css";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import { AuthProvider } from '../context/auth'
-
-
-
+import { AuthProvider } from '../context/auth';
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "SPA - Sentirse bien",
-  description: "¡Saca tu turno ahora!",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname(); // Obtiene la ruta actual
+
+  // Verifica si la ruta actual comienza con lo que quiero que no muestre el NavBar y el Footer
+  const shouldHideNavFooter = pathname.startsWith("/proyecto2");
+
   return (
     <html lang="es">
       <body className={inter.className}>
         <AuthProvider>
-            <NavBar/>
-              {children}
-            <Footer/>
+          {!shouldHideNavFooter && <NavBar />}
+            {children}
+          {!shouldHideNavFooter && <Footer />}
         </AuthProvider>
-
       </body>
       <link rel="icon" href="logo_sin_fondo.png" sizes="any" />
     </html>
---
+++ src/app/proyecto2/page.tsx
@@ -0,0 +1,9 @@
+
+export default function proyecto2() {
+    return (
+      <div>
+        <h1>Contenido de PageL sin Navbar ni Footer</h1>
+      </div>
+    );
+  }
+  
