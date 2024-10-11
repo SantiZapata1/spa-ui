e0b911fdae93bfe9774628a49917cc6a44bf98a9
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2024-10-11 19:40:20 -0300

    Cambios menores

--- src/app/proyecto2/page.tsx
+++ src/app/proyecto2/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAuth } from '../../context/auth';
-import MenuLateral2 from '../components/Menu/menuLateral2'; // Importamos el nuevo componente
+import MenuLateral2 from '../components/Menu/menuLateral2'; 
 
 export default function Proyecto2() {
   const { user, isAuthenticated } = useAuth();
@@ -11,16 +11,16 @@ export default function Proyecto2() {
     <div className="relative min-h-screen flex">
       {/* Menú lateral */}
       <div className="absolute top-0 left-0">
-        <MenuLateral2 /> {/* Utilizamos el nuevo menú lateral aquí */}
+        <MenuLateral2 /> 
       </div>
 
       {/* Contenido principal */}
-      <div className="flex-grow ml-64 p-8 text-center"> {/* Margen para compensar el menú lateral */}
+      <div className="flex-grow ml-64 p-8 text-center"> 
         <h1 className="text-4xl font-bold mb-4">
           Bienvenido, {role}
         </h1>
         <main className="relative z-10 mb-32">
-          {/* Aquí iría el contenido dinámico dependiendo de la ruta */}
+          
         </main>
       </div>
     </div>
